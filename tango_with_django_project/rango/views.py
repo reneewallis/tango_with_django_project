969e5c8fba0fa714446959ce9c322b6fdb00897b
Renee Wallis <2560885w@student.gla.ac.uk>
2023-01-17 15:36:12 +0000

    added hyperlinks and about page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner!" +"<br> </br>" + "<a href='/rango/about/'>About</a>")
 
+def about(request):
+    return HttpResponse("Rango says here is the about page." + "<br> </br>" +"<a href='/rango/'>Index</a>")
